1e3dd8b34039b6483b470030d99d73edde6d6251
dokterkepin <dokterkepin@icloud.com>
2023-06-13 20:53:29 +0800

    Contoh Library sederhana Composer

---
+++ src/Costumer.php
@@ -0,0 +1,11 @@
+<?php
+class Costumer{
+    public function __construct(private string $name)
+    {
+        
+    }
+
+    public function panggil($name): string{
+        return  "Hello $name, my name is $this->name";
+    }
+}
